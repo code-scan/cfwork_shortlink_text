dca91844ccd132daecb3bc7267ef21246f6553e5
cond0r@desktop <zlc@zhao.cl>
2022-05-03 18:46:27 +0800

    add-icon

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const index = `<!doctype html>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>ShortLink</title>
+    <title>🔗 ShortLink</title>
 </head>
 
 <style>
